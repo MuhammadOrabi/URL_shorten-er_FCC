36410d43acfce50f26ada5cd426473d6999f0bfc
Muhammad Orabi <muhammad.orabi.2014@hotmail.com>
2017-02-15 14:17:11 +0200

    add postgrest for production

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 // Create new SQL LITE DB
-
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-	'dialect': 'sqlite',
-	'storage': __dirname + '/data/dev-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'develpment';
+var sequelize;
+if (env === 'develpment') {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-api.sqlite'
+	});
+}else {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		'dialect': 'postgres'
+	});
+}
 var db = {};
 db.url = sequelize.import(__dirname + '/models/urls.js');
 db.sequelize = sequelize;
